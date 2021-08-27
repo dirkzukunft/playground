d6ec1a0cf1b6c4443294e9bccae851cdf6e7927e
dirkzukunft <git@qf3.de>
2021-08-28 00:13:03 +0200

    Add Unsplash and Pixabay

--- src/api/images.ts
+++ src/api/images.ts
@@ -5,13 +5,32 @@ dotenv.config();
 
 const apis = [
   {
+    key: process.env.KEY_UNSPLASH,
+    url: `https://api.unsplash.com/search/photos?query=pizza`,
+    resultKeys: {
+      count: 'total',
+      results: 'results',
+    },
+  },
+  {
     key: process.env.KEY_PEXELS,
     url: `https://api.pexels.com/v1/search?query=pizza`,
+    resultKeys: {
+      count: 'total_results',
+      results: 'photos',
+    },
+  },
+  {
+    url: `https://pixabay.com/api/?key=${process.env.KEY_PIXABAY}&q=pizza`,
+    resultKeys: {
+      count: 'total',
+      results: 'hits',
+    },
   },
 ];
 
 export async function images(_req: Request, res: Response): Promise<void> {
-  const api = apis[0];
+  const api = apis[2];
   try {
     const response = await fetch(api.url, {
       headers: { Authorization: api.key || `` },
@@ -19,7 +38,8 @@ export async function images(_req: Request, res: Response): Promise<void> {
     });
     try {
       const result = await response.json();
-      res.status(200).json(result);
+      const resultsCount = result[api.resultKeys.count];
+      res.status(200).json(resultsCount);
     } catch {
       res.status(503).send('');
     }
