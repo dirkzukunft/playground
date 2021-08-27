568d8fe139310ea77423a0c78bfeb6b0bfe90ebc
dirkzukunft <git@qf3.de>
2021-08-27 23:46:10 +0200

    Add timeout and error handling to fetch

--- src/api/images.ts
+++ src/api/images.ts
@@ -3,16 +3,27 @@ import { Request, Response } from 'express';
 import fetch from 'node-fetch';
 dotenv.config();
 
-const apis = {
-  key: process.env.KEY_PEXELS,
-  url: `https://api.pexels.com/v1/search?query=pizza`,
-};
+const apis = [
+  {
+    key: process.env.KEY_PEXELS,
+    url: `https://api.pexels.com/v1/search?query=pizza`,
+  },
+];
 
 export async function images(_req: Request, res: Response): Promise<void> {
-  const response = await fetch(apis.url, {
-    headers: { Authorization: apis.key || `` },
-  });
-  const result = await response.json();
-
-  res.status(200).json(result);
+  const api = apis[0];
+  try {
+    const response = await fetch(api.url, {
+      headers: { Authorization: api.key || `` },
+      timeout: 2000,
+    });
+    try {
+      const result = await response.json();
+      res.status(200).json(result);
+    } catch {
+      res.status(503).send('');
+    }
+  } catch {
+    res.status(504).send('');
+  }
 }
