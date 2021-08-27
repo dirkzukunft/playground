e44875ea0ad671f374722c659113f67cba976289
dirkzukunft <git@qf3.de>
2021-08-27 22:34:22 +0200

    Add Fonts List

---
+++ src/app/components/Fonts/Fonts.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Fonts from './Fonts';
+
+export default {
+  title: 'Component/Fonts',
+  component: Fonts,
+};
+
+export const list = (): JSX.Element => <Fonts />;
