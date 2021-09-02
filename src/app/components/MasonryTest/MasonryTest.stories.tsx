4ce97c640dac7ba47570407f47abdf9a8ae82b48
dirkzukunft <87307560+dirkzukunft@users.noreply.github.com>
2021-09-02 23:24:52 +0200

    masonry (#5)

---
+++ src/app/components/MasonryTest/MasonryTest.stories.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import MasonryTest from './MasonryTest';
+export default {
+  title: 'Component/MasonryTest',
+  component: MasonryTest,
+};
+
+export const masonryTest = (): JSX.Element => <MasonryTest />;
